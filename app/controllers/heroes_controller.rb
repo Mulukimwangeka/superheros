8ecc657feebe1201a9187f3638603df675f85234
mulukimwangeka <mwangekaruth@gmail.com>
2022-12-07 01:36:02 +0300

    controllers done

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,2 +1,15 @@
 class HeroesController < ApplicationController
+    def index
+        render json: Hero.all
+    end
+
+    def show
+        hero = Hero.find_by(id: params[:id])
+        if hero
+            render json: hero
+        else
+            render json: {error: "Hero not found"}, status: 404
+        end
+
+    end
 end
